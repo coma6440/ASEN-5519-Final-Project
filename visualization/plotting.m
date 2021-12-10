69ec00e85ded5f507cf2f5978c8ba4386d2436f6
Conner Martin <coma6440@colorado.edu>
2021-12-09 23:12:46 -0700

    Started matlab plotting

--- visualization/plotting.m
+++ visualization/plotting.m
@@ -5,18 +5,18 @@ clc
 addpath(genpath('../lib/yaml'));
 %% Load in files
 sol_folder = "../solutions/kinodynamic";
-ws_folder = "../configs";
 sol_listing = dir(sol_folder);
-ws_listing = dir(ws_folder);
 sol_listing([sol_listing.isdir]) = [];
-ws_listing([ws_listing.isdir]) = [];
-ws_listing(~contains({ws_listing.name},"yaml")) = [];
+
 for i = 1:length(sol_listing)
+    env = strsplit(sol_listing(i).name, "_");
+    env = env{1}; % Get just the workspace
+    env = ReadYaml(['../configs/', env ,'.yaml']);
     A = readmatrix(sol_folder + filesep + sol_listing(i).name, 'OutputType', 'double');
 end
 
 figure
-plot3(A(:,1), A(:,2), A(:,3))
+plot3(A(:,1), A(:,2), A(:,3),'k', 'LineWidth', 2)
 grid on
 xlim([0,15])
 ylim([-10,10])
@@ -24,3 +24,44 @@ zlim([-10,10])
 xlabel("X Position")
 ylabel("Y Position")
 zlabel("Z Position")
+
+hold on
+plot_obstacles(env.obstacles)
+plot_goal(env.goal)
+
+%% Helper Functions
+function c = cuboid(size, position, orientation)
+c =  [-size(1)/ 2, -size(2) / 2, -size(3) / 2;
+    -size(1) / 2, -size(2) / 2, size(3) / 2;
+    -size(1) / 2, +size(2) / 2, -size(3) / 2;
+    -size(1) / 2, +size(2) / 2, size(3) / 2;
+    size(1) / 2, -size(2) / 2, -size(3) / 2;
+    size(1) / 2, -size(2) / 2, size(3) / 2;
+    size(1) / 2, +size(2) / 2, -size(3) / 2;
+    size(1) / 2, +size(2) / 2, size(3) / 2];
+for i = 1:length(c)
+    c(i,:) = quatrotate([orientation(end), orientation(1:3)],c(i,:)) + position;
+end
+end
+
+function plot_obstacles(obs_struct)
+obs = fieldnames(obs_struct);
+for i = 1:length(obs)
+    o = obs_struct.(obs{i});
+    p = cell2mat(o.position);
+    q = cell2mat(o.orientation);
+    s = cell2mat(o.size);
+    c = cuboid(s, p, q);
+    [k,~] = convhull(c(:,1),c(:,2),c(:,3),'Simplify',true);
+    trisurf(k,c(:,1),c(:,2),c(:,3),'FaceColor','red', 'FaceAlpha', 0.4)
+end
+end
+
+function plot_goal(g)
+    p = cell2mat(g.position);
+    q = cell2mat(g.orientation);
+    s = cell2mat(g.size);
+    c = cuboid(s, p, q);
+    [k,~] = convhull(c(:,1),c(:,2),c(:,3),'Simplify',true);
+    trisurf(k,c(:,1),c(:,2),c(:,3),'FaceColor','green','FaceAlpha', 0.4)
+end
