15b1c680d309c03821e8cf51ce9e54cd2b4bb47e
Conner Martin <conner@pop-os.localdomain>
2021-12-10 13:51:26 -0700

    saving

--- src/kinodynamic.cpp
+++ src/kinodynamic.cpp
@@ -58,7 +58,7 @@ void planWithSimpleSetup(std::vector<std::shared_ptr<fcl::CollisionObjectf>> obs
     // Set state validity checking for this space
     oc::SpaceInformation* si = ss.getSpaceInformation().get();
     ss.setStateValidityChecker([si, obstacles, robot](const ob::State* state) { return isStateValid(si, state, obstacles, robot); });
-    si->setStateValidityCheckingResolution(0.001); // 1%
+    // si->setStateValidityCheckingResolution(0.001); // 1%
 
     // Use the ODESolver to propagate the system. Call PostIntegration when done
     auto odeSolver(std::make_shared<oc::ODEBasicSolver<>>(ss.getSpaceInformation(), &DynamicsODE));
@@ -71,7 +71,7 @@ void planWithSimpleSetup(std::vector<std::shared_ptr<fcl::CollisionObjectf>> obs
     ss.setup();
 
     // Solve the planning problem
-    ob::PlannerStatus solved = ss.solve(120);
+    ob::PlannerStatus solved = ss.solve(180);
     if (solved && ss.haveExactSolutionPath())
         {
         // TODO: Save the path
