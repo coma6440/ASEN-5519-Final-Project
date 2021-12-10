660fdf67b5bb7e38ec908e87d5f3f140ef70e846
Conner Martin <coma6440@colorado.edu>
2021-12-09 23:21:50 -0700

    Got some more plotting done

--- visualization/plotting.m
+++ visualization/plotting.m
@@ -1,4 +1,4 @@
-%% Housekeeping 
+%% Housekeeping
 close all
 clear
 clc
@@ -13,21 +13,26 @@ for i = 1:length(sol_listing)
     env = env{1}; % Get just the workspace
     env = ReadYaml(['../configs/', env ,'.yaml']);
     A = readmatrix(sol_folder + filesep + sol_listing(i).name, 'OutputType', 'double');
+    
+    plot_position(A)
+    plot_orientation(A)
+    
+    figure
+    plot3(A(:,1), A(:,2), A(:,3),'k', 'LineWidth', 2)
+    grid on
+    xlim([0,15])
+    ylim([-10,10])
+    zlim([-10,10])
+    xlabel("X Position")
+    ylabel("Y Position")
+    zlabel("Z Position")
+    
+    hold on
+    plot_obstacles(env.obstacles)
+    plot_goal(env.goal)
 end
 
-figure
-plot3(A(:,1), A(:,2), A(:,3),'k', 'LineWidth', 2)
-grid on
-xlim([0,15])
-ylim([-10,10])
-zlim([-10,10])
-xlabel("X Position")
-ylabel("Y Position")
-zlabel("Z Position")
 
-hold on
-plot_obstacles(env.obstacles)
-plot_goal(env.goal)
 
 %% Helper Functions
 function c = cuboid(size, position, orientation)
@@ -58,10 +63,62 @@ end
 end
 
 function plot_goal(g)
-    p = cell2mat(g.position);
-    q = cell2mat(g.orientation);
-    s = cell2mat(g.size);
-    c = cuboid(s, p, q);
-    [k,~] = convhull(c(:,1),c(:,2),c(:,3),'Simplify',true);
-    trisurf(k,c(:,1),c(:,2),c(:,3),'FaceColor','green','FaceAlpha', 0.4)
+p = cell2mat(g.position);
+q = cell2mat(g.orientation);
+s = cell2mat(g.size);
+c = cuboid(s, p, q);
+[k,~] = convhull(c(:,1),c(:,2),c(:,3),'Simplify',true);
+trisurf(k,c(:,1),c(:,2),c(:,3),'FaceColor','green','FaceAlpha', 0.4)
+end
+
+function plot_position(A)
+t = cumsum(A(:,end));
+figure
+tiledlayout(4,1);
+nexttile
+plot(t,A(:,1))
+grid on
+ylabel("X Position")
+
+nexttile
+plot(t,A(:,2))
+grid on
+ylabel("Y Position")
+
+nexttile
+plot(t,A(:,3))
+grid on
+ylabel("Z Position")
+
+nexttile
+plot(t,A(:,8))
+grid on
+ylabel("Speed")
+xlabel("Time [s]")
+end
+
+function plot_orientation(A)
+t = cumsum(A(:,end));
+figure
+tiledlayout(4,1)
+nexttile
+plot(t,A(:,4))
+grid on
+ylabel("q_x")
+
+nexttile
+plot(t,A(:,5))
+grid on
+ylabel("q_y")
+
+nexttile
+plot(t,A(:,6))
+grid on
+ylabel("q_z")
+
+nexttile
+plot(t,A(:,7))
+grid on
+xlabel("Time [s]")
+ylabel("q_w")
 end
