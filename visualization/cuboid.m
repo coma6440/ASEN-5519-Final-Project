58e218652dd0b4d147f5c9203182bc35cc7ab163
Conner Martin <coma6440@colorado.edu>
2021-12-11 18:27:03 -0700

    fixed orientation

--- visualization/cuboid.m
+++ visualization/cuboid.m
@@ -7,5 +7,9 @@ c =  [-size(1)/ 2, -size(2) / 2, -size(3) / 2;
     size(1) / 2, -size(2) / 2, size(3) / 2;
     size(1) / 2, +size(2) / 2, -size(3) / 2;
     size(1) / 2, +size(2) / 2, size(3) / 2];
-    c = quatrotate(orientation([4,1:3]),c) + position;
+    q = quaternion(orientation([4,1:3]));
+    for i = 1:length(c)
+        c(i,:) = rotatepoint(q, c(i,:)) + position;
+    end
+%     c = quatrotate(orientation([4,1:3]),c) + position;
 end
